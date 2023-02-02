e46f8fe90bd5b6a273f58a0c867d936144b536ec
Steven Lawson <steven.lawson@healthsqyre.com>
2023-02-02 11:59:54 -0700

    finished component story states and added accessibility a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,15 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
-  framework: "@storybook/vue3",
+  framework: '@storybook/vue3',
   core: {
-    builder: "@storybook/builder-webpack5",
+    builder: '@storybook/builder-webpack5',
   },
   features: {
     interactionsDebugger: true,
